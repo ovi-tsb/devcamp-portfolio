c400d0d21dae5e9e6fe4a67dc70d0fbf6c87cda5
Ovidiu Turcu <ovi_tsb@yahoo.com>
2021-11-15 13:14:12 -0500

    Implemented defaults for portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,13 @@ class Portfolio < ApplicationRecord
 	end
 
 	scope :ruby_on_rails_portfolio_items, -> {where(subtitle: "Ruby on Rails")}
+
+	after_initialize :set_defaults
+
+	def set_defaults
+		self.main_image ||= "https://via.placeholder.com/600x400.png?%20C/O%20https://placeholder.com/"
+		self.thumb_image ||= "https://via.placeholder.com/350x150.png?%20C/O%20https://placeholder.com/"
+	end
+
 end
+
