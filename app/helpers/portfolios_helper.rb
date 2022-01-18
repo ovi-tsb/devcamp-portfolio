1d25f74883e6323350002b201272a4a997928b4f
Ovidiu Turcu <ovi_tsb@yahoo.com>
2022-01-18 11:57:46 -0500

    Updated file upload forms and refactor placeholder functionality into view

--- app/helpers/portfolios_helper.rb
+++ app/helpers/portfolios_helper.rb
@@ -1,2 +1,20 @@
 module PortfoliosHelper
+	def image_generator(height:, width:)
+		"https://via.placeholder.com/#{height}x#{width}.png?%20C/O%20https://placeholder.com/"
+	end
+
+	def portfolio_img img, type
+		if img.model.main_image? || img.model.thumb_image?
+			img
+		elsif type == 'thumb'
+			image_generator(height: '350', width: '200')
+		else type == 'main'
+			image_generator(height: '600', width: '400')
+		end
+
+	end
+
 end
+
+
+
--- app/models/concerns/placeholder.rb
+++ app/models/concerns/placeholder.rb
@@ -1,7 +1,3 @@
 module Placeholder
 	extend ActiveSupport::Concern
-
-	def self.image_generator(height:, width:)
-		"https://via.placeholder.com/#{height}x#{width}.png?%20C/O%20https://placeholder.com/"
-	end
 end
